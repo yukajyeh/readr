c5e06c08f928b4db7aac2e63cb20f30cadd46e73
allecramarcella <allecramarcella.dev@gmail.com>
2020-11-25 15:58:43 +0100

    responsive nav bar with icons when screen small

--- front-end/src/components/navbar/Navbar.js
+++ front-end/src/components/navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Link, NavLink } from 'react-router-dom';
 import './Navbar.css'
-import LogoIconLight from '../../assets/logo/icon_orange.png'
+import LogoBlue from '../../assets/logo/logo_light_background.png'
 import IconProfile from '../../assets/icons/profile_orangecolor.png'
 import IconMatches from '../../assets/icons/book_heart_orange.png'
 import IconRate from '../../assets/icons/swipe_orange.png'
@@ -31,56 +31,49 @@ export default class Navbar extends Component {
 
 
     render() {
-   
-        if(!this.state.loggedInUser){
+
+        if(this.state.loggedInUser){
             return(
                 <nav className="nav-style">
                     <div className='nav-left'> 
-                        <Link to='/' ><img className='logo-icon' src={LogoIconLight} alt='Logo readr'/></Link>
-                        <ul>
-                            <li className='nav-icon'> <NavLink to='/find-my-match' activeClassName='selected'><img src={IconRate} alt='icon profile' /></NavLink></li>
-                            <li className='nav-icon'> <NavLink to='/find-my-match' activeClassName='selected' ><img src={IconProfile} alt='icon profile' /></NavLink></li>
-                            <li className='nav-icon'> <NavLink to='/find-my-match' activeClassName='selected' ><img src={IconMatches} alt='icon profile' /></NavLink></li>
+      
+                        <NavLink to='/profile' activeClassName='selected' className='nav-icon' ><img src={IconProfile} alt='icon profile' /></NavLink>
+                        <NavLink to='/find-my-match' activeClassName='selected' className='nav-icon'><img src={IconRate} alt='icon profile' /></NavLink>
+                        <NavLink to='/matches' activeClassName='selected' className='nav-icon'><img src={IconMatches} alt='icon profile' /></NavLink>
 
-                            <li> <NavLink to='/find-my-match' activeClassName='selected' className='nav-text-link'>Find my match</NavLink></li>
-                            <li> <NavLink to='/profile' activeClassName='selected' className='nav-text-link'>Profile</NavLink></li>
-                            <li> <NavLink to='/matches' activeClassName='selected' className='nav-text-link'>Matches</NavLink></li>
-  
-                        </ul>
+                        <div className='nav-big-screen'>
+                            <Link to='/' className='logo-icon'><img  src={LogoBlue} alt='Logo readr'/></Link>
+                            
+                            <NavLink to='/find-my-match' activeClassName='selected'>Find my match</NavLink>
+                            <NavLink to='/profile' activeClassName='selected'>Profile</NavLink>
+                            <NavLink to='/matches' activeClassName='selected'>Matches</NavLink>
+                        </div>
+                       
                     </div>
-                    <div>
-                        <ul>
-                            <li><NavLink to='/' onClick={this.logoutUser}>Logout</NavLink></li>
-                        </ul>
+                    <div className='nav-right'>
+                    
+                        <NavLink to='/' onClick={this.logoutUser}>Logout</NavLink>
+                  
                     </div>
                 </nav>
             )
         } else {
-            if(window.location.href.includes('about') || window.location.href.includes('404')) {
                 return (
                     <nav className="nav-style">
-                        <div> <Link to='/'><img className='logo-icon' src={LogoIconLight} alt='Logo readr'/></Link></div>
-                        <div>
-                            <ul>
-                                <li><NavLink to='/login' activeClassName='selected' >Log in</NavLink></li>
-                                <li><NavLink to='/signup' activeClassName='selected'>Sign up</NavLink></li>
-                            </ul>
+                        <div className='nav-left'> 
+                            <div className='nav-big-screen'>
+                                <Link to='/' className='logo-icon'><img  src={LogoBlue} alt='Logo readr'/></Link>
+                            </div>
+                        </div>    
+                        <div className='nav-right-no-user'>
+                            <NavLink to='/login' activeClassName='selected' >Log in</NavLink>
+                            <NavLink to='/signup' activeClassName='selected'>Sign up</NavLink>
+                            <NavLink to='/about' activeClassName='selected' >About</NavLink>
                         </div>
                     </nav>
                 )
-            }  else {
-                return (
-                    <nav className="nav-style">
-                        <div> </div>
-                        <div>
-                            <ul>
-                                <li><NavLink to='/login' activeClassName='selected'>Log in</NavLink></li>
-                                <li><NavLink to='/about' activeClassName='selected'>About</NavLink></li>
-                            </ul>
-                        </div>
-                    </nav>
-                )
-            }
+                
+            
 
         }
     }
