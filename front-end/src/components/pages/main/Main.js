8fbee83f7ea638d03fe036c9ccfbfab64d1f736a
allecramarcella <allecramarcella.dev@gmail.com>
2020-12-02 16:04:29 +0100

    added about section nav needs to be updated

--- front-end/src/components/pages/main/Main.js
+++ front-end/src/components/pages/main/Main.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Element } from "react-scroll"
 import "./Main.css"
 import AuthService from "../../../services/auth/auth-services"
 import LogoDarkBackground from '../../../assets/logo/Logo_dark_background.png'
@@ -7,17 +8,27 @@ import Button from "../../elements/button/Button"
 
 
 
+
 export default function Main() {
     return (
-        <div className='main-container'>
-            <div className='first-container'>
-                <img src={LogoDarkBackground} alt='Logo readr' />
-                <TurnedCorner position='home'/>
+        <div>
+            <div className='main-container'>
+                <div className='first-container'>
+                    <img src={LogoDarkBackground} alt='Logo readr' />
+                    <TurnedCorner position='home'/>
+                </div>
+                <div className='second-container'>
+                    <h1>Book<br/>Your Next<br/>Date</h1>
+                    <Button><a href='/signup'>Sign up</a></Button>
+                </div>
             </div>
-            <div className='second-container'>
-                <h1>Book <br/>Your Next  <br/> Date</h1>
-                <Button><a href='/signup'>Sign up</a></Button>
-            </div>
+            <Element id='about-info' name='about-info'>
+                <article className='about-section'>
+                    <h2>About</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent non vestibulum neque. Praesent sit amet egestas eros. Morbi tempor ac eros vitae tincidunt. Nulla tincidunt malesuada tincidunt. Suspendisse placerat maximus arcu a scelerisque.  </p>
+                    <p>Mauris mauris enim, pharetra nec lacinia sed, vehicula at enim. Nunc pulvinar tristique nisi, non feugiat neque blandit et. Nullam tincidunt dignissim diam, eu fringilla arcu rutrum ut. Nulla euismod sollicitudin sagittis. Proin egestas sapien id enim dictum feugiat. Aenean porttitor ornare purus, nec convallis massa ultrices a. Nam turpis libero, pharetra eget mi a, congue consectetur metus.</p>
+                </article>
+            </Element>
         </div>
     )
 }
