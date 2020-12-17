c80eb7874abefa43732c8b880aff6413fd7351c7
allecramarcella <allecramarcella.dev@gmail.com>
2020-12-17 16:07:39 +0100

    added trust proxy

--- back-end/configs/session.config.js
+++ back-end/configs/session.config.js
@@ -3,12 +3,22 @@ const MongoStore = require('connect-mongo')(session);
 const mongoose = require('mongoose');
  
 module.exports = app => {
+
+  const isDevMode = process.env.ENV === 'development'
+
+  if (!isDevMode) {
+    app.set('trust proxy', 1);
+  }
+
   app.use(
     session({
       secret: process.env.SESSION_SECRET,
       resave: false,
       saveUninitialized: true,
-      cookie: { maxAge: 60000 * 100},
+      cookie: { 
+        maxAge: 60000 * 100,
+        secure: !isDevMode,
+      },
       store: new MongoStore({
         mongooseConnection: mongoose.connection,
         ttl: 60 * 60 * 24 
